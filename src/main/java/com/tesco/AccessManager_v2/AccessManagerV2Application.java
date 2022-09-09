f5419e2d170cf0c96b43bb2bc75100208149d987
Andrew Joseph <andrew.joseph2@tesco.com>
2022-09-09 18:16:21 +0530

    Add files via upload

---
+++ src/main/java/com/tesco/AccessManager_v2/AccessManagerV2Application.java
@@ -0,0 +1,13 @@
+package com.tesco.AccessManager_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessManagerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessManagerV2Application.class, args);
+	}
+
+}
